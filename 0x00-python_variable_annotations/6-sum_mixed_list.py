fc89a6514b894f3e56b94d547087f9cd74a62e69
Oluwapelumi Fadolapo <pelumifadolapo7@gmail.com>
2023-04-27 15:13:55 +0100

    Added file for task 6

---
+++ 0x00-python_variable_annotations/6-sum_mixed_list.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+""" Module for task 6
+"""
+
+from typing import List, Union
+
+
+def sum_mixed_list(mxd_lst: List[Union[int, float]]) -> float:
+    """ Method to return sum of list of ints and floats
+    """
+    return sum(mxd_lst)
