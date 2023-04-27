0d2353517f5f2f433a3d10c29a2451e46ef21f9d
Oluwapelumi Fadolapo <pelumifadolapo7@gmail.com>
2023-04-27 16:36:56 +0100

    Added file for task 10

---
+++ 0x00-python_variable_annotations/100-safe_first_element.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+""" Module for task 9
+"""
+
+from typing import Sequence, Union, Any
+
+
+# The types of the elements of the input are not known
+def safe_first_element(lst: Sequence[Any]) -> Union[Any, None]:
+    """ Return the first element if the list exists, else None
+    """
+    if lst:
+        return lst[0]
+    else:
+        return None
