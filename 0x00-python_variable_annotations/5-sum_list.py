affd3aba057a4dbbf160e14b9b6ac1efef2889f7
Oluwapelumi Fadolapo <pelumifadolapo7@gmail.com>
2023-04-27 15:09:37 +0100

    Added files for task 5

---
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+""" Module for task 5
+"""
+
+from typing import List
+
+
+def sum_list(input_list: List[float]) -> float:
+    """ Method to return sum of list of floats
+    """
+    return sum(input_list)
