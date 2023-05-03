7da83ebb7b1309724afae268c23fdeb54b1727b5
Oluwapelumi Fadolapo <pelumifadolapo7@gmail.com>
2023-05-03 22:30:12 +0100

    Pycodestyle fix for task 0

--- 0x02-python_async_comprehension/1-async_comprehension.py
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -5,9 +5,10 @@
 import asyncio
 from random import uniform
 from typing import Generator
+async_generator = __import__('0-async_generator').async_generator
 
 
-async def async_generator() -> Generator[float, None, None]:
+async def async_generator() -> Generator[int, None, None]:
     """ Async Generator method
     """
     for i in range(10):
