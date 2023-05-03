c2fc0bcf68fe58c816e6b29b51219faedff9c810
Oluwapelumi Fadolapo <pelumifadolapo7@gmail.com>
2023-05-03 05:56:41 +0100

    Pycodestyle fix for task 4

--- 0x01-python_async_function/4-tasks.py
+++ 0x01-python_async_function/4-tasks.py
@@ -9,8 +9,9 @@ task_wait_random = __import__('3-tasks').task_wait_random
 
 
 async def task_wait_n(n: int, max_delay: int) -> List[float]:
-    """ Function to generate random delay using asyncio n times
+    """ Generate random delay using asyncio n times using task_wait_random
     """
     arr: List[float] = []
-    arr = await asyncio.gather(*(task_wait_random(max_delay) for i in range(n)))
+    arr = await asyncio.gather(*(task_wait_random(max_delay)
+                                 for i in range(n)))
     return sorted(arr)
