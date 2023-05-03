d44b9a94fb475260870ee5242f4d5ee1a101ab95
Oluwapelumi Fadolapo <pelumifadolapo7@gmail.com>
2023-05-03 05:18:35 +0100

    Added files for task 1

---
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+""" Module for task 1
+"""
+
+import asyncio
+from random import uniform
+from typing import List
+
+wait_random = __import__('0-basic_async_syntax').wait_random
+
+
+async def wait_n(n: int, max_delay: int) -> List[float]:
+    """ Function to generate random delay using asyncio n times
+    """
+    arr: List[float] = await asyncio.gather(*(wait_random(max_delay) for i in range(n)))
+    return sorted(arr)
