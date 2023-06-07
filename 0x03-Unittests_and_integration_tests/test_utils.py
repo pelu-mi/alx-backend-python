a51ad7cd8dd800b7c27fbd3cf87ea6446aa78d64
Oluwapelumi Fadolapo <pelumifadolapo7@gmail.com>
2023-06-07 07:13:41 +0100

    Added changes for task 1

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -9,6 +9,7 @@ from typing import Dict, Tuple, Union
 # Import methods to test
 from utils import access_nested_map
 
+
 class TestAccessNestedMap(unittest.TestCase):
     """ Testing utils.access_nested_map
     """
@@ -25,3 +26,18 @@ class TestAccessNestedMap(unittest.TestCase):
             ) -> None:
         """ Test output for utils.access_nested_map"""
         self.assertEqual(access_nested_map(nested_map, path), expected)
+
+    @parameterized.expand([
+        ({}, ("a",), "a"),
+        ({"a": 1}, ("a", "b"), "b"),
+    ])
+    def test_access_nested_map_exception(
+            self,
+            nested_map: Dict,
+            path: Tuple[str],
+            msg: str
+            ) -> None:
+        """ Test exception raised for access nested map """
+        with self.assertRaises(KeyError) as err:
+            access_nested_map(nested_map, path)
+        self.assertEqual(str(err.exception), msg)
